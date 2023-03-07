e48dd4511ca8bbb4bfd94d1499223f01aadefa4e
Tate Redding <tateredding@gmail.com>
2023-03-07 09:09:53 -0700

    cleanup in prep for part 2

--- db/seed.js
+++ db/seed.js
@@ -72,9 +72,21 @@ const createTables = async () => {
 const createInitialUsers = async () => {
     try {
         console.log('Creating users...');
-        await createUser({ username: 'albert', password: 'bertie99', name: 'Al Bert', location: 'Sidney, Australia' });
-        await createUser({ username: 'sandra', password: '2sandy4me', name: 'Just Sandra', location: "Ain't Tellin'" });
-        await createUser({ username: 'glamgal', password: 'soglam', name: 'Joshua', location: 'Upper East Side' });
+        await createUser({
+            username: 'albert',
+            password: 'bertie99',
+            name: 'Al Bert',
+            location: 'Sidney, Australia' });
+        await createUser({
+            username: 'sandra',
+            password: '2sandy4me',
+            name: 'Just Sandra',
+            location: "Ain't Tellin'" });
+        await createUser({
+            username: 'glamgal',
+            password: 'soglam',
+            name: 'Joshua',
+            location: 'Upper East Side' });
         console.log('Finished creating users.');
     } catch (error) {
         console.error('Error when creating users!');
@@ -88,27 +100,21 @@ const createInitialPosts = async () => {
         const [albert, sandra, glamgal] = await getAllUsers();
         await createPost({
             authorId: albert.id,
-            title: "First Post",
-            content: "This is my first post. I hope I love writing blogs as much as I love writing them.",
+            title: 'First Post',
+            content: 'This is my first post. I hope I love writing blogs as much as I love writing them.',
             tags: ['#happy', '#youcandoanything']
         });
         await createPost({
-            authorId: albert.id,
-            title: "Another Post",
-            content: "I realize my first post made no sense. Unfortunatley, that's how the FSA workshop told me to do it!",
+            authorId: sandra.id,
+            title: 'How does this work?',
+            content: 'Seriously, does this even do anything?',
             tags: ['#happy', '#worst-day-ever']
         });
         await createPost({
-            authorId: sandra.id,
-            title: "Sanda's Post",
-            content: "This is Sandra's post",
-            tags: ['#sandra', '#canhumansdoeverything']
-        });
-        await createPost({
             authorId: glamgal.id,
-            title: "GlamGal's Post",
-            content: "Here we are again!",
-            tags: ['#happy', '#youcandoanything', '#canhumansdoeverything']
+            title: 'Living the Glam Life',
+            content: 'Do you even? I swear that half of you are posing.',
+            tags: ['#happy', '#youcandoanything', '#canmandoeverything']
         });
         console.log('Finished creating posts.');
     } catch (error) {
@@ -117,30 +123,6 @@ const createInitialPosts = async () => {
     };
 };
 
-const createInitialTags = async () => {
-    try {
-        console.log('Creating tags...');
-
-        const [happy, sad, inspo, catman] = await createTags([
-            '#happy',
-            '#worst-day-ever',
-            '#youcandoanything',
-            '#catmandoeverything'
-        ]);
-
-        const [postOne, postTwo, postThree] = await getAllPosts();
-
-        await addTagsToPost(postOne.id, [happy, inspo]);
-        await addTagsToPost(postTwo.id, [sad, inspo]);
-        await addTagsToPost(postThree.id, [happy, catman, inspo]);
-
-        console.log('Finished creating tags.');
-    } catch (error) {
-        console.log('Error when creating tags!');
-        throw error;
-    }
-}
-
 const rebuildDB = async () => {
     try {
         client.connect();
@@ -148,7 +130,6 @@ const rebuildDB = async () => {
         await createTables();
         await createInitialUsers();
         await createInitialPosts();
-        // await createInitialTags();
     } catch (error) {
         console.error(error);
     };
@@ -164,8 +145,8 @@ const testDB = async () => {
 
         console.log('Updating first user...');
         const updateUserResult = await updateUser(users[0].id, {
-            name: 'Tate Redding',
-            location: 'Fort Collins, CO'
+            name: 'Newname Sogood',
+            location: 'Lesterville, KY'
         });
         console.log('Result: ', updateUserResult);
 
